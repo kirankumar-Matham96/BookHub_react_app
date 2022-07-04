47b2b7030fb3ab68a2f47b974cfa9a1bf3c3e662
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2022-07-04 14:11:51 +0000

    feature: footer completed

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,11 @@
 import Header from '../Header'
+import Footer from '../Footer'
 import './index.css'
 
 const Home = () => (
   <div className="home-bg-container">
     <Header />
+    <Footer />
   </div>
 )
 
